f0875d90b12ba0908c0daac935cdbf72b100fefd
Deinyefa Eporwei <df.eporwei@gmail.com>
2019-11-25 12:12:10 -0500

    run build

---
+++ client/build/precache-manifest.58035f076ef76abbb9d9183e6ac9559b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d652e33dcccf511a99ef9aadf6519d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee6619673bdeca3516ff",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "836e514d88a54f077bac",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ee6619673bdeca3516ff",
+    "url": "/static/js/2.16072330.chunk.js"
+  },
+  {
+    "revision": "836e514d88a54f077bac",
+    "url": "/static/js/main.02d39831.chunk.js"
+  },
+  {
+    "revision": "f1c6019deaec6f4a9d0b",
+    "url": "/static/js/runtime-main.11bc70d3.js"
+  }
+]);
