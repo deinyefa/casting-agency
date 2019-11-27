c47e5ba6700eba10693d6cb0b4db0421dcfe190a
Deinyefa Eporwei <df.eporwei@gmail.com>
2019-11-27 08:25:49 -0500

    rebuild, move .env, rebuild

---
+++ client/build/precache-manifest.2c1d9abf0af5d831a4e9dc27ab5036a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60d2817750e6212dd719a373dd2283ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae26a6ebee77bd6659cb",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "a88f01dc0223213040f7",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ae26a6ebee77bd6659cb",
+    "url": "/static/js/2.b4ce2089.chunk.js"
+  },
+  {
+    "revision": "a88f01dc0223213040f7",
+    "url": "/static/js/main.d6452b31.chunk.js"
+  },
+  {
+    "revision": "f1c6019deaec6f4a9d0b",
+    "url": "/static/js/runtime-main.11bc70d3.js"
+  }
+]);
