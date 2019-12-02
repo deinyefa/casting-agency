cbc937272a95aa212927278e0fd412e4a6c1665b
Deinyefa Eporwei <df.eporwei@gmail.com>
2019-12-02 06:46:55 -0500

    Update auth_config.js

--- client/src/utils/auth_config.js
+++ client/src/utils/auth_config.js
@@ -5,7 +5,6 @@ export const REACT_APP_CALLBACKURL = process.env.REACT_APP_CALLBACKURL;
 export const REACT_APP_URL = process.env.REACT_APP_URL;
 export const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL
 
-
 // export const REACT_APP_DOMAIN='dev-deinyefa.auth0.com'
 // export const REACT_APP_URL='dev-deinyefa'
 // export const REACT_APP_CLIENTID='gZlfS8C3GgADzGL5UezkiwTTkNrPl5bR'
